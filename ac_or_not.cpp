35d8c09b54c9ffef882fc111189b750c84152361
Pankaj Singh <53267754+pankajsingh09@users.noreply.github.com>
2021-01-03 22:37:10 +0530

    Add files via upload

---
+++ ac_or_not.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+char a[100];
+int n,i;
+ 
+int main()
+ {
+     cout<<"\n Enter String";
+     cin>>a;
+     n=strlen(a);
+
+     if(a[0]=='a' && (a[n-1]=='a' || a[n-1]=='b') && a[n-2]=='c')
+     {
+         for(i=1;i<n-2;i++)
+         {
+             if(a[i]!='b')
+             {
+                 cout<<"\n string is not acceptable";
+                 exit(0);
+             }
+         }
+         cout<<"\n sting is acceptable";
+     }
+     else
+     {
+         cout<<"\n string is not acceptable";
+     }
+     
+     return 0;
+     
+ }
